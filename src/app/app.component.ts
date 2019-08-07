574cfe2e31c1a563d54f8460b0d515af6e23743d
Jaume Parrot Altisent <jaumeparrotaltisent@MacBook-Pro-de-Jaume.local>
2019-08-07 22:08:28 +0200

    sistema de literales

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'softack-overflow';
+  public activeLang = 'es';
+
+  constructor(private translate: TranslateService) {
+    this.translate.setDefaultLang(this.activeLang);
+
+    translate.use(this.activeLang);
+  }
+
+  public cambiarLenguaje(lang) {
+    this.activeLang = lang;
+    this.translate.use(lang);
+  }
 }
