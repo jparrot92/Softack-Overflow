9540cb4456ad3319cc151470606c83ebcb85843f
Jaume Parrot Altisent <jaumeparrotaltisent@MacBook-Pro-de-Jaume.local>
2019-08-10 01:08:19 +0200

    Añadiendo modelo respuesta

---
+++ src/app/answer/answer.ts
@@ -0,0 +1,17 @@
+import { Question } from '../question/question';
+
+export class User {
+  constructor(
+    public firstName: string,
+    public lastName: string
+  ) { }
+}
+
+export class Answer {
+  constructor(
+    public description: string,
+    public question: Question,
+    public createdAt?: Date,
+    public user?: User
+  ) { }
+}
--- src/app/question/question.ts
+++ src/app/question/question.ts
@@ -1,19 +1,22 @@
+import { Answer } from '../answer/answer';
+
 export class Question {
   title: string;
   description: string;
   createdAt?: Date;
   icon?: string;
+  answers: Answer[];
 
   constructor(
     title: string,
     description: string,
     createdAt?: Date,
-    icon?: string
-  ) {
+    icon?: string) {
+
     this.title = title;
     this.description = description;
     this.createdAt = createdAt;
     this.icon = icon;
+    this.answers = [];
   }
-
 }
