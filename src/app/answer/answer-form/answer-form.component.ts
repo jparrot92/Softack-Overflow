22fa894c19609be607aae47bef4240023d34fcd9
Jaume Parrot Altisent <jaumeparrotaltisent@MacBook-Pro-de-Jaume.local>
2019-08-10 18:02:57 +0200

    Mostrar respuesta en el listado

--- src/app/answer/answer-form/answer-form.component.ts
+++ src/app/answer/answer-form/answer-form.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Answer, User } from '../answer';
+import { Question } from '@app/question/question';
 
 @Component({
   selector: 'app-answer-form',
@@ -8,8 +10,16 @@ import { NgForm } from '@angular/forms';
 })
 
 export class AnswerFormComponent {
-  onSubmit(form: NgForm) {
-    console.log(form.value.description);
+  @Input() question: Question;
 
+  onSubmit(form: NgForm) {
+    const answer = new Answer(
+      form.value.description,
+      this.question,
+      new Date(),
+      new User('Albert', 'Cosme')
+    );
+    this.question.answers.unshift(answer);
+    form.reset();
   }
 }
