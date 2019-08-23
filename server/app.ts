35e2406f3387051c7545dd79300a343e301ee3a5
Jaume Parrot Altisent <jaumeparrotaltisent@MacBook-Pro-de-Jaume.local>
2019-08-24 00:17:24 +0200

    Configuracion CORS

--- server/app.ts
+++ server/app.ts
@@ -8,6 +8,7 @@ export class App {
   constructor(private port?: number | string) {
     this.app = express();
     this.settings();
+    this.cors();
     this.routes();
   }
 
@@ -17,6 +18,17 @@ export class App {
 
   private routes() {
     Routes.init(this.app);
+  }
+
+  private cors() {
+    if (process.env.NODE_ENV === 'development') {
+      this.app.use((req, res, next) => {
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Request-With, Content-Type, Accept');
+        res.setHeader('Access-Control-Allow-Methods', 'POST, GET, PATCH, DELETE, OPTIONS');
+        next();
+      });
+    }
   }
 
   async listen() {
