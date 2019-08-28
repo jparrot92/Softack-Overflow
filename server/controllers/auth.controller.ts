dc5e89daa1d47fa6216da2b69d9a37ad23b98788
Jaume Parrot Altisent <jaumeparrotaltisent@MacBook-Pro-de-Jaume.local>
2019-08-29 00:21:05 +0200

    Creacion JWT para el autenticado del usuario

--- server/controllers/auth.controller.ts
+++ server/controllers/auth.controller.ts
@@ -1,4 +1,7 @@
 import { Request, Response } from 'express';
+import jwt from 'jsonwebtoken';
+
+const secret = 'miclavesecreta';
 
 export const users = [
   {
@@ -39,9 +42,17 @@ export async function signin(req: Request, res: Response): Promise<Response | vo
       });
     }
 
+    const token = jwt.sign({ user }, secret, { expiresIn: 86400 });
+    res.status(200).json({
+      message: 'Login succede',
+      token,
+      userId: user._id,
+      firstName: user.firstName,
+      lastName: user.lastName,
+      email: user.email
+  });
+
   } catch (e) {
     console.log(e);
   }
 }
-
-
